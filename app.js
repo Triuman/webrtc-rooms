7861763090901f9fb202d249899e89ce4a467573
Triuman <alfanzo27@hotmail.com>
2021-03-20 14:30:00 +0300

    Added WebRTC features (not working yet)

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-var path = require('path');
+const path = require('path');
 
 app.use(express.static('public'));
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 const users = {};
-const rooms = {
-  asd: true,
-  grgwef: true,
-  cwocwecewcew: true
-};
+const rooms = {};
 
 io.on('connection', (socket) => {
   users[socket.id] = socket;
@@ -38,9 +33,9 @@ io.on('connection', (socket) => {
     rooms[roomId] = true;
   });
 
-  socket.on('offer', (offer) => socket.to(socket.roomId).emit('offer', { id: socket.id, offer}));
-  socket.on('answer', ({ id, answer }) => users[id].emit('answer', { id: socket.id, answer }));
-
+  socket.on('offer', (offer) => socket.to(socket.roomId).emit('offer', { id: socket.id, offer }));
+  socket.on('answer', ({ id, answer }) => socket.to(id).emit('answer', { id: socket.id, answer }));
+  socket.on('ice', ({ id, candidate }) => socket.to(id).emit('ice', { id: socket.id, candidate }));
 });
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
--- public/index.js
+++ public/index.js
@@ -5,53 +5,116 @@ let divRoomList;
 let divRooms;
 let txtRoomId;
 let divRoom;
+let divCameras;
 
 let socket;
+let peerConnections = {};
+let localOffer;
+let localStream;
+const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
 
-window.onload = () => {
-    txtUsername = document.getElementById('txtUsername');
-    divConnecting = document.getElementById('divConnecting');
-    divLogin = document.getElementById('divLogin');
-    divRooms = document.getElementById('divRooms');
-    divRoomList = document.getElementById('divRoomList');
-    txtRoomId = document.getElementById('txtRoomId');
-    divRoom = document.getElementById('divRoom');
+window.onload = async () => {
+  txtUsername = document.getElementById('txtUsername');
+  divConnecting = document.getElementById('divConnecting');
+  divLogin = document.getElementById('divLogin');
+  divRooms = document.getElementById('divRooms');
+  divRoomList = document.getElementById('divRoomList');
+  txtRoomId = document.getElementById('txtRoomId');
+  divRoom = document.getElementById('divRoom');
+  divCameras = document.getElementById('divCameras');
 
   hideAllViews();
+  await getLocalStream();
 
   socket = io();
 
   socket.on('connect', () => {
     divLogin.style.display = 'block';
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    console.log(socket.id);
   });
 
   socket.on('disconnect', () => {
     hideAllViews();
     divConnecting.style.display = 'block';
   });
-  
-  socket.on('roomlist', roomList => {
+
+  socket.on('roomlist', (roomList) => {
     hideAllViews();
     divRoomList.innerHTML = '';
-    roomList.forEach(roomId => {
-        divRoomList.innerHTML += '<div><a href="#" onclick="enterRoom(\'' + roomId + '\')">' + roomId + '</a></div>';
+    roomList.forEach((roomId) => {
+      divRoomList.innerHTML += '<div><a href="#" onclick="enterRoom(\'' + roomId + '\')">' + roomId + '</a></div>';
     });
     divRooms.style.display = 'block';
   });
 
-  socket.on('joinedroom', () => {
-    hideAllViews();    
+  socket.on('joinedroom', async () => {
+    hideAllViews();
     divRoom.style.display = 'block';
-    // TODO: Create an offer and send to the server.
+    // We create this PeerConnection just to get an offer.
+    const pc = new RTCPeerConnection([configuration]);
+    localOffer = await pc.createOffer();
+    socket.emit('offer', localOffer);
+    console.log('Sent the offer to the room.');
   });
-  socket.on('offer', () => {
-    // TODO: create an answer and send it to server
+  socket.on('offer', async ({ id, offer }) => {
+    peerConnections[id] = new RTCPeerConnection([configuration]);
+    addPCEventListeners(id);
+    localStream.getTracks().forEach((track) => {
+      peerConnections[id].addTrack(track, localStream);
+    });
+    await peerConnections[id].setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await peerConnections[id].createAnswer();
+    await peerConnections[id].setLocalDescription(answer);
+    socket.emit('answer', { id, answer });
+    console.log('Sent answer to user ' + id);
   });
-  socket.on('answer', () => {
-    // TODO: create a new webrtc connection using this answer
+  socket.on('answer', async ({ id, answer }) => {
+    peerConnections[id] = new RTCPeerConnection([configuration]);
+    addPCEventListeners(id);
+    localStream.getTracks().forEach((track) => {
+      peerConnections[id].addTrack(track, localStream);
+    });
+    await peerConnections[id].setLocalDescription(localOffer);
+    await peerConnections[id].setRemoteDescription(new RTCSessionDescription(answer));
+  });
+  socket.on('ice', async ({ id, candidate }) => {
+    try {
+      console.log('Adding ICE candidate for user ' + id);
+      await peerConnections[id].addIceCandidate(candidate);
+    } catch (e) {
+      console.error('Error adding received ice candidate', e);
+    }
   });
 };
+
+function addPCEventListeners(id) {
+  peerConnections[id].addEventListener('connectionstatechange', (event) => {
+    if (peerConnections[id].connectionState === 'connected') {
+      // Peers connected!
+      console.log('Connected to peer ' + id);
+    }
+  });
+  peerConnections[id].addEventListener('icecandidate', (event) => {
+    if (event.candidate) {
+      console.log('Got local ICE candidates.');
+      socket.emit('ice', { id, candidate: event.candidate });
+    }
+  });
+  const remoteStream = new MediaStream();
+  const remoteVideo = document.createElement('video');
+  remoteVideo.srcObject = remoteStream;
+  remoteVideo.autoplay = true;
+  divCameras.appendChild(remoteVideo);
+  peerConnections[id].addEventListener('track', async (event) => {
+    remoteStream.addTrack(event.track, remoteStream);
+  });
+}
+
+async function getLocalStream() {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  const localVideo = document.getElementById('localVideo');
+  localVideo.srcObject = localStream;
+}
 
 function login() {
   const username = txtUsername.value;
@@ -60,18 +123,17 @@ function login() {
 }
 
 function enterRoom(roomId) {
-    socket.emit('enterroom', roomId);
-  }
-  
-function createRoom() {
-    const roomId = txtRoomId.value;
-    socket.emit('enterroom', roomId);
-  }
-  
+  socket.emit('enterroom', roomId);
+}
 
-function hideAllViews(){
-    divConnecting.style.display = 'none';
-    divLogin.style.display = 'none';
-    divRooms.style.display = 'none';
-    divRoom.style.display = 'none';
+function createRoom() {
+  const roomId = txtRoomId.value;
+  socket.emit('enterroom', roomId);
+}
+
+function hideAllViews() {
+  divConnecting.style.display = 'none';
+  divLogin.style.display = 'none';
+  divRooms.style.display = 'none';
+  divRoom.style.display = 'none';
 }
